39834eb68e32752d471068b4d345a8975f70e752
olhaSabadina <oll04181527@gmail.com>
2023-05-30 11:57:08 +0300

    addUITests

--- CurrencyConverterUITests/CurrencyConverterUITests.swift
+++ CurrencyConverterUITests/CurrencyConverterUITests.swift
@@ -6,36 +6,54 @@
 //
 
 import XCTest
+@testable import CurrencyConverter
 
 final class CurrencyConverterUITests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    func testPossibilityInputAndDeleteValue() {
         let app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let tablesQuery = app.tables
+        tablesQuery.cells.containing(.staticText, identifier:"UAH  ").element.tap()
+        XCTAssert(tablesQuery.buttons["Clear text"].exists)
+        tablesQuery.buttons["Clear text"].tap()
+        XCTAssertEqual(tablesQuery.cells.containing(.staticText, identifier:"UAH  ").element.value as! String, "")
+        XCTAssert(app.toolbars["Toolbar"].buttons["Done"].exists)
+        tablesQuery.cells.containing(.staticText, identifier:"UAH  ").element.typeText("2000")
+        XCTAssert(tablesQuery.buttons["Clear text"].exists)
+        app.toolbars["Toolbar"].buttons["Done"].tap()
+        app/*@START_MENU_TOKEN@*/.buttons["Buy"]/*[[".segmentedControls.buttons[\"Buy\"]",".buttons[\"Buy\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app/*@START_MENU_TOKEN@*/.buttons["Sell"]/*[[".segmentedControls.buttons[\"Sell\"]",".buttons[\"Sell\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
     }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    
+    func testExistsCurrenciesListTable() {
+        let app = XCUIApplication()
+        app.launch()
+        app/*@START_MENU_TOKEN@*/.staticTexts["Add Currency"]/*[[".buttons[\"Add Currency\"].staticTexts[\"Add Currency\"]",".staticTexts[\"Add Currency\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        XCTAssert(app.staticTexts["Currencies List"].exists)
+        app.tables.children(matching: .cell).element(boundBy: 0).tap()
+    }
+        
+    func testCurrencyTableDeleteCurrency() {
+        testExistsCurrenciesListTable()
+        let app = XCUIApplication()
+        app.launch()
+        let tablesQuery = app.tables
+        tablesQuery.children(matching: .cell).element(boundBy: 2).tap()
+        let window = app.children(matching: .window).element(boundBy: 0)
+        let element = window.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element
+        element.swipeLeft()
+        XCTAssert(tablesQuery/*@START_MENU_TOKEN@*/.buttons["Delete"]/*[[".cells.buttons[\"Delete\"]",".buttons[\"Delete\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.exists)
+        tablesQuery/*@START_MENU_TOKEN@*/.buttons["Delete"]/*[[".cells.buttons[\"Delete\"]",".buttons[\"Delete\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+    }
+    
+    func testCurrencyTableUsingNationalBankExchangeRate() {
+        let app = XCUIApplication()
+        app.launch()
+        XCTAssert(app.staticTexts["National Bank Exchange Rate"].exists)
+        app.staticTexts["National Bank Exchange Rate"].tap()
+        app.buttons["OK"].tap()
+        XCTAssert(app.staticTexts["Return to course PB"].exists)
+        app.staticTexts["Return to course PB"].tap()
     }
 }
